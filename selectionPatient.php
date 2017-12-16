40d6305633c8e20714cc2a8c0629f106713764e5
HS20 <33666062+HS20@users.noreply.github.com>
2017-12-16 12:31:48 +0100

    Create selectionPatient.php

---
+++ selectionPatient.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+session_start();
+
+// CONNECTION A LA BASE
+
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=kc', 'root', '',		 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+	die('Erreur :'.$e->getMessage());
+};
+
+
+
+$_SESSION['numpat'] = $_POST['Numpat'];
+
+
+// REQUETE DE SELECTION DE TOUS LES NUMERO DOSSIER
+
+$requete= "select Num_dossier FROM tab_patient WHERE Num_dossier =".$_SESSION['numpat'].";";
+$resultat=$bdd->query($requete);
+$ligne = $resultat->fetch();
+
+
+// SI NUMERO DE DOSSIER TROUVE -> Menu choix de modification
+
+ if ($_SESSION['numpat'] == $ligne['Num_dossier'])
+ 	{	
+ 		
+ 		
+ 		echo "Dossier Trouvé";
+ 		echo'<ul id="sous_menu">
+             <li>
+               <a href="Deletechir.html">Suppression Antécedent chirurgical</a>
+             </li>
+             <li>
+               <a href=#>xxx</a>
+             </li>
+             <li>
+               <a href=#>xxx</a>
+             </li>
+             ';
+
+ 	}
+	else
+	{
+	echo "Dossier non trouvé " ;
+	}
+
+
+$resultat->closeCursor();
+
+// RETOUR MENU
+
+echo '<br /><a href="selectionPatient.html">Retour à la saisie du numero de dossier</a><br />';
+echo '<a href="xxxx">Retour a la page principale</a>';
+
+?>
+
+</body>
+</html>

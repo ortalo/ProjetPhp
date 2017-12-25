fe35f3e2b994bd31797a8d40d16c8390dc063d61
ortalo <pierre.ortalo@gmail.com>
2017-12-25 09:14:42 +0100

    Update modifGrossesse.php
    retiré le mot de passe

--- modifGrossesse.php
+++ modifGrossesse.php
@@ -16,7 +16,7 @@ if (isset($_POST['cleGrossesse'])){
 if (isset($_POST['newGrossesse'])){
     if(ctype_digit ($_POST['newGrossesse']) && strlen ( $_POST['newGrossesse'])==4 ){
         try{
-           $bdd = new PDO('mysql:host=localhost;dbname=kc', 'root', 'root', 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
+           $bdd = new PDO('mysql:host=localhost;dbname=kc', 'root', '', 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
             $reqMaxIdGrosesse = "SELECT max(idGrossesse) FROM tab_grossesse WHERE Num_Dossier = '".$_SESSION['numpat']."';";
             $resultatMaxId=$bdd->query($reqMaxIdGrosesse);
             $ligne = $resultatMaxId->fetch();
@@ -52,7 +52,7 @@ if (isset($_POST['newGrossesse'])){
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=kc', 'root', 'root', 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=kc', 'root', '', 	array(PDO::ATTR_ERRMODE => 	PDO::ERRMODE_EXCEPTION));
     $resultat=$bdd->query("SELECT Num_Dossier, idGrossesse, Annee FROM tab_grossesse WHERE Num_Dossier = '".$_SESSION['numpat']."';");
     
     while ($ligne = $resultat->fetch()){ 
